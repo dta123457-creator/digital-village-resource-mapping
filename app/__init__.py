3e21df81ae4be009e833c624b22c907aa22c9252
dta123457-creator <dta123457@gmail.com>
2026-07-11 19:55:05 +0530

    Add app package initialization

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Digital Village Resource Mapping Application
+AI-powered GIS platform for Smart India Hackathon
+"""
+
+__version__ = "0.1.0"
+__author__ = "dta123457-creator"
+__description__ = "AI-powered GIS platform for Smart India Hackathon - Village resource mapping and infrastructure analysis"
